1e87aa413e860a487d55b0e0a17176aef05bf46a
Yi Tseng <a86487817@gmail.com>
2016-04-27 16:16:55 +0800

    Use map to store relationship for better scalability

--- K2B.java
+++ K2B.java
@@ -5,4 +5,49 @@ class K2B{
 
 		return String.format("%c%s", ch, (str.length()>1) ? k2b(str.substring(1)) : "");
 	}
+    private void initKBMap() {
+        this.kbMap = new HashMap<>();
+        kbMap.put('1', 'ㄅ');
+        kbMap.put('q', 'ㄆ');
+        kbMap.put('a', 'ㄇ');
+        kbMap.put('z', 'ㄈ');
+        kbMap.put('2', 'ㄉ');
+        kbMap.put('w', 'ㄊ');
+        kbMap.put('s', 'ㄋ');
+        kbMap.put('x', 'ㄌ');
+        kbMap.put('e', 'ㄍ');
+        kbMap.put('d', 'ㄎ');
+        kbMap.put('c', 'ㄏ');
+        kbMap.put('r', 'ㄐ');
+        kbMap.put('f', 'ㄑ');
+        kbMap.put('v', 'ㄒ');
+        kbMap.put('5', 'ㄓ');
+        kbMap.put('t', 'ㄔ');
+        kbMap.put('g', 'ㄕ');
+        kbMap.put('b', 'ㄖ');
+        kbMap.put('y', 'ㄗ');
+        kbMap.put('h', 'ㄘ');
+        kbMap.put('n', 'ㄙ');
+        kbMap.put('u', 'ㄧ');
+        kbMap.put('j', 'ㄨ');
+        kbMap.put('m', 'ㄩ');
+        kbMap.put('8', 'ㄚ');
+        kbMap.put('i', 'ㄛ');
+        kbMap.put('k', 'ㄜ');
+        kbMap.put(',', 'ㄝ');
+        kbMap.put('9', 'ㄞ');
+        kbMap.put('o', 'ㄟ');
+        kbMap.put('l', 'ㄠ');
+        kbMap.put('.', 'ㄡ');
+        kbMap.put('0', 'ㄢ');
+        kbMap.put('p', 'ㄣ');
+        kbMap.put(';', 'ㄤ');
+        kbMap.put('/', 'ㄥ');
+        kbMap.put('-', 'ㄦ');
+        kbMap.put('6', 'ˊ');
+        kbMap.put('3', 'ˇ');
+        kbMap.put('4', 'ˋ');
+        kbMap.put('7', '˙');
+        kbMap.put(' ', ' ');
+    }
 }
