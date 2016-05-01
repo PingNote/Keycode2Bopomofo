3d19d3012848548aa7a34104ea8206fd89169560
Yi Tseng <a86487817@gmail.com>
2016-05-01 18:32:46 +0800

    Add auto input function

--- src/main/java/k2b/K2B.java
+++ src/main/java/k2b/K2B.java
@@ -1,14 +1,21 @@
 package k2b;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class K2B {
 
     private Map<Character, Character> kbMap;
+    private Map<Character, Integer> kbVkMap;
 
     public K2B() {
         this.initKBMap();
+        this.initkbVkMap();
     }
 
     private void initKBMap() {
@@ -57,6 +64,53 @@ public class K2B {
         kbMap.put(' ', ' ');
     }
 
+    private void initkbVkMap() {
+        this.kbVkMap = new HashMap<>();
+        kbVkMap.put('1', KeyEvent.VK_1);
+        kbVkMap.put('q', KeyEvent.VK_Q);
+        kbVkMap.put('a', KeyEvent.VK_A);
+        kbVkMap.put('z', KeyEvent.VK_Z);
+        kbVkMap.put('2', KeyEvent.VK_2);
+        kbVkMap.put('w', KeyEvent.VK_W);
+        kbVkMap.put('s', KeyEvent.VK_S);
+        kbVkMap.put('x', KeyEvent.VK_X);
+        kbVkMap.put('e', KeyEvent.VK_E);
+        kbVkMap.put('d', KeyEvent.VK_D);
+        kbVkMap.put('c', KeyEvent.VK_C);
+        kbVkMap.put('r', KeyEvent.VK_R);
+        kbVkMap.put('f', KeyEvent.VK_F);
+        kbVkMap.put('v', KeyEvent.VK_V);
+        kbVkMap.put('5', KeyEvent.VK_5);
+        kbVkMap.put('t', KeyEvent.VK_T);
+        kbVkMap.put('g', KeyEvent.VK_G);
+        kbVkMap.put('b', KeyEvent.VK_B);
+        kbVkMap.put('y', KeyEvent.VK_Y);
+        kbVkMap.put('h', KeyEvent.VK_H);
+        kbVkMap.put('n', KeyEvent.VK_N);
+        kbVkMap.put('u', KeyEvent.VK_U);
+        kbVkMap.put('j', KeyEvent.VK_J);
+        kbVkMap.put('m', KeyEvent.VK_M);
+        kbVkMap.put('8', KeyEvent.VK_8);
+        kbVkMap.put('i', KeyEvent.VK_I);
+        kbVkMap.put('k', KeyEvent.VK_K);
+        kbVkMap.put(',', KeyEvent.VK_COMMA);
+        kbVkMap.put('9', KeyEvent.VK_9);
+        kbVkMap.put('o', KeyEvent.VK_O);
+        kbVkMap.put('l', KeyEvent.VK_L);
+        kbVkMap.put('.', KeyEvent.VK_PERIOD);
+        kbVkMap.put('0', KeyEvent.VK_0);
+        kbVkMap.put('p', KeyEvent.VK_P);
+        kbVkMap.put(';', KeyEvent.VK_SEMICOLON);
+        kbVkMap.put('/', KeyEvent.VK_SLASH);
+        kbVkMap.put('-', KeyEvent.VK_MINUS);
+        kbVkMap.put('6', KeyEvent.VK_6);
+        kbVkMap.put('3', KeyEvent.VK_3);
+        kbVkMap.put('4', KeyEvent.VK_4);
+        kbVkMap.put('7', KeyEvent.VK_7);
+        kbVkMap.put(' ', KeyEvent.VK_SPACE);
+
+    }
+
     public String generateK2b(String kbChars) {
         StringBuilder sb = new StringBuilder();
 
@@ -69,18 +123,45 @@ public class K2B {
         return sb.toString();
     }
 
+    public void autoInput(String kbChars) {
+
+        try {
+            Robot robot = new Robot();
+            robot.delay(1000);
+            kbChars.chars().forEachOrdered((val) -> {
+                char c = (char) val;
+                int key = kbVkMap.get(c);
+                robot.keyPress(key);
+                robot.keyRelease(key);
+            });
+        } catch (AWTException ex) {
+            Logger.getLogger(K2B.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     public static void usage() {
         System.out.println("Usage: ");
-        System.out.println("\tK2B [your keyboard string]");
+        System.out.println("\tK2B [--auto-input] [your keyboard string]");
     }
 
     public static void main(String[] args) {
         K2B k2bGenerator = new K2B();
+
         if (args.length == 0) {
             usage();
-        } else {
-            String result = k2bGenerator.generateK2b(args[0]);
+        } else if(args.length == 1){
+            String kbString = String.join(" ", args);
+            String result = k2bGenerator.generateK2b(kbString);
             System.out.println(result);
+        } else {
+          String[] tmpStrings = new String[args.length-1];
+          System.arraycopy(args, 1, tmpStrings, 0, args.length-1);
+          String kbString = String.join(" ", tmpStrings);
+            if(args[0].equals("--auto-input")) {
+                k2bGenerator.autoInput(kbString);
+            } else {
+
+            }
         }
 
     }
